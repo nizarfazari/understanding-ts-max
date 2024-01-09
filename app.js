f1eec5ed6c625e365e64c8a5c67376081f39b586
Muhammad Nizar Fazari <nexsel32@gmail.com>
2024-01-09 17:33:02 +0700

    trying work with number,boolean,string

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 console.log("Your code goes here");
-function add(n1, n2) {
-    return n1 + n2;
+function add(n1, n2, showResult, phrasa) {
+    // jika mengggunakan javascript
+    //   if (typeof n1 !== "number" && typeof n1 !== "number") {
+    //     throw new Error("Incorect input!");
+    //   }
+    if (showResult) {
+        console.log(phrasa + n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-console.log(add(number1, number2));
+var showResult = true;
+var resultPhrase = "Result is : ";
+console.log(add(number1, number2, showResult, resultPhrase));
--- app.ts
+++ app.ts
@@ -1,14 +1,22 @@
 console.log("Your code goes here");
 
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrasa: string) {
   // jika mengggunakan javascript
   //   if (typeof n1 !== "number" && typeof n1 !== "number") {
   //     throw new Error("Incorect input!");
   //   }
-  return n1 + n2;
+  const result = n1 + n2;
+  if (showResult) {
+    // semuanya akan di konversi ke string hasilnya : 52.8
+    // console.log(phrasa + n1 + n2);
+    console.log(phrasa + result);
+  } else {
+    return result;
+  }
 }
 
 const number1 = 5;
 const number2 = 2.8;
-
-console.log(add(number1, number2));
+const showResult = true;
+const resultPhrase = "Result is : ";
+console.log(add(number1, number2, showResult, resultPhrase));
