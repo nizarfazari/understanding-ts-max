b83f1f0563b72b18d0218726c10cae76c723a13d
Muhammad Nizar Fazari <nexsel32@gmail.com>
2024-01-12 00:07:00 +0700

    arrow function in ts

--- app.ts
+++ app.ts
@@ -1,5 +1,22 @@
+const userName = "Nizar";
+
+// Function
+const addFunction = function () {};
+
+// Arrow Function
+const addArrowFunction = (a: number , b : number) => {
+  return a + b;
+};
+
+const printOutput: (a: number | string) => void = (output) =>
+  (console.log(output))
+
 const button = document.querySelector("button");
 
-button?.addEventListener("click", () => {
-  console.log("asdas");
-});
+if (button) {
+  button.addEventListener("click", (event) => console.log(event));
+}
+
+
+printOutput(addArrowFunction(5, 5))
+
