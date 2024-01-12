08d5b1e3f4dd17c9b008d7eb3b2369a2e435f671
Muhammad Nizar Fazari <nexsel32@gmail.com>
2024-01-12 16:09:56 +0700

    Inheritance pada typescript

--- app.ts
+++ app.ts
@@ -17,15 +17,44 @@ class Departement {
   }
 }
 
-const accounting = new Departement("2", "Accounting");
+class ITDepartment extends Departement {
+  admins: string[];
 
-accounting.addEmployee("Nizar");
-accounting.addEmployee("Fazari");
+  // selama tidak membuat konstruktor maka akan menggunakan constructor super class
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    // this harus di tulis setelah super
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Departement {
+  constructor(id: string, private reports: string[] = []) {
+    super(id, "Accounting");
+  }
+
+  addReports(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("2", ["Nizar"]);
+
+it.addEmployee("Nizar");
+it.addEmployee("Fazari");
+it.describe();
+it.printEmployeeInformation();
+console.log(it);
+
+const accounting = new AccountingDepartment("4");
+accounting.addReports("asdas");
+console.log(accounting);
+
 
 // accounting.employee[2] = "Anna"; => jika ingin class tidak bisa di akses dari luar, tambahkan access modifier private pada propery
-
-accounting.describe();
-accounting.printEmployeeInformation();
-
 // const accountingCoppy = { name : "asd" ,describe: accounting.describe };
 // accountingCoppy.describe(); => akan error karena mengacu pada Kelas Object accountingCoppy jadinya harus menambahkan sebuah property
--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,12 @@
 "use strict";
 class Departement {
-    constructor(n) {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
         this.employee = [];
-        this.name = n;
     }
     describe() {
-        console.log("Department : " + this.name);
+        console.log(`Department ( ${this.id} ) : ` + this.name);
     }
     addEmployee(employee) {
         this.employee.push(employee);
@@ -15,11 +16,35 @@ class Departement {
         console.log(this.employee);
     }
 }
-const accounting = new Departement("Accounting");
-accounting.addEmployee("Nizar");
-accounting.addEmployee("Fazari");
+class ITDepartment extends Departement {
+    // selama tidak membuat konstruktor maka akan menggunakan constructor super class
+    constructor(id, admins) {
+        super(id, "IT");
+        // this harus di tulis setelah super
+        this.admins = admins;
+    }
+}
+class AccountingDepartment extends Departement {
+    constructor(id, reports = []) {
+        super(id, "Accounting");
+        this.reports = reports;
+    }
+    addReports(text) {
+        this.reports.push(text);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+const it = new ITDepartment("2", ["Nizar"]);
+it.addEmployee("Nizar");
+it.addEmployee("Fazari");
+it.describe();
+it.printEmployeeInformation();
+console.log(it);
+const accounting = new AccountingDepartment("4");
+accounting.addReports("asdas");
+console.log(accounting);
 // accounting.employee[2] = "Anna"; => jika ingin class tidak bisa di akses dari luar, tambahkan access modifier private pada propery
-accounting.describe();
-accounting.printEmployeeInformation();
 // const accountingCoppy = { name : "asd" ,describe: accounting.describe };
 // accountingCoppy.describe(); => akan error karena mengacu pada Kelas Object accountingCoppy jadinya harus menambahkan sebuah property
