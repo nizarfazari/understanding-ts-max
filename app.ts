b73bbcb1fa33f69908ac8fea969da09ed28ab3fb
Muhammad Nizar Fazari <nexsel32@gmail.com>
2024-01-16 02:15:57 +0700

    working w decorator factories

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
-function Logger(constructor: Function) {
-  console.log("Logging");
-  console.log(constructor);
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log("Logging");
+    console.log(constructor);
+  };
 }
 
-@Logger
+@Logger("Haiii semuanya")
 class Person {
   name = "Nizar";
   constructor() {
