1865853e1334f323fb8cda1e64e8be6652fd5e7b
Muhammad Nizar Fazari <nexsel32@gmail.com>
2024-01-18 21:07:45 +0700

    learn auto bind pada ts
    Autobinding pada decorator di TypeScript adalah teknik di mana metode-metode dalam kelas secara otomatis diikat ke instance kelas saat instansiasi, sehingga this dalam metode tersebut selalu merujuk pada instance yang sesuai

--- app.ts
+++ app.ts
@@ -7,14 +7,23 @@ function Logger(logString: string) {
 }
 
 function withTemplate(template: string, hookId: string) {
-  return function (constructor: any) {
-    console.log('With template')
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
-    }
+  // ini seperti membuat kembali class baru, karena decorator di bentuk saat di definisikannya sebuah class
+  // sehingga jika kita membuat function ini akan terbentuk jika telah di buatnya sebuah class
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(...args: any[]) {
+        console.log(args[0], "argumen");
+        super();
+        console.log("With template");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
   };
 }
 
@@ -22,10 +31,107 @@ function withTemplate(template: string, hookId: string) {
 @withTemplate("<h1> My Person Object </h1>", "app")
 class Person {
   name = "Nizar";
-  constructor() {
+  constructor(angka: number) {
     console.log("Creating Constructor");
   }
 }
 
-const pers = new Person();
-console.log(pers);
+const test = new Person(2);
+console.log(test);
+
+function Log(target: any, properyName: string | symbol) {
+  console.log("Property Decorator");
+  console.log(target, properyName);
+}
+
+function Log2(
+  target: any,
+  properyName: string,
+  descriptor: TypedPropertyDescriptor<number>
+) {
+  console.log("Accessor Decorator");
+  console.log(target);
+  console.log(properyName);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  properyName: string | symbol,
+  descriptor: TypedPropertyDescriptor<(tax: number) => number>
+) {
+  console.log("Methhod Decorator");
+  console.log(target);
+  console.log(properyName);
+  console.log(descriptor);
+}
+
+function Log4(target: any, properyName: string | symbol, position: number) {
+  console.log("Parameter Decorator");
+  console.log(target);
+  console.log(properyName);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  // karena  yang bisa di edit adalah price lebih enak di tambahkan _
+  _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
+new Product("asdsa", 2);
+new Product("asdsa", 4);
+
+function Autobind(
+  target: any,
+  methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDecriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+
+  return adjDecriptor;
+}
+
+class Printer {
+  _message = "This works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this._message);
+  }
+}
+
+const p = new Printer();
+p.showMessage();
+
+const button2 = document.querySelector("button");
+// karena addEventListener mengikat method showMessage sehingga this itu merefer ke addEventListerner
+button2?.addEventListener("click", p.showMessage);
--- dist/app.js
+++ dist/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Logger(logString) {
     return function (constructor) {
         console.log(logString);
@@ -13,18 +16,25 @@ function Logger(logString) {
     };
 }
 function withTemplate(template, hookId) {
-    return function (constructor) {
-        console.log('With template');
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector("h1").textContent = p.name;
-        }
+    // ini seperti membuat kembali class baru, karena decorator di bentuk saat di definisikannya sebuah class
+    // sehingga jika kita membuat function ini akan terbentuk jika telah di buatnya sebuah class
+    return function (originalConstructor) {
+        return class extends originalConstructor {
+            constructor(...args) {
+                console.log(args[0], "argumen");
+                super();
+                console.log("With template");
+                const hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector("h1").textContent = this.name;
+                }
+            }
+        };
     };
 }
 let Person = class Person {
-    constructor() {
+    constructor(angka) {
         this.name = "Nizar";
         console.log("Creating Constructor");
     }
@@ -33,5 +43,84 @@ Person = __decorate([
     Logger("Haiii semuanya"),
     withTemplate("<h1> My Person Object </h1>", "app")
 ], Person);
-const pers = new Person();
-console.log(pers);
+const test = new Person(2);
+console.log(test);
+function Log(target, properyName) {
+    console.log("Property Decorator");
+    console.log(target, properyName);
+}
+function Log2(target, properyName, descriptor) {
+    console.log("Accessor Decorator");
+    console.log(target);
+    console.log(properyName);
+    console.log(descriptor);
+}
+function Log3(target, properyName, descriptor) {
+    console.log("Methhod Decorator");
+    console.log(target);
+    console.log(properyName);
+    console.log(descriptor);
+}
+function Log4(target, properyName, position) {
+    console.log("Parameter Decorator");
+    console.log(target);
+    console.log(properyName);
+    console.log(position);
+}
+class Product {
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error("Invalid price - should be positive!");
+        }
+    }
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
+__decorate([
+    Log2
+], Product.prototype, "price", null);
+__decorate([
+    Log3,
+    __param(0, Log4)
+], Product.prototype, "getPriceWithTax", null);
+new Product("asdsa", 2);
+new Product("asdsa", 4);
+function Autobind(target, methodName, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDecriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        },
+    };
+    return adjDecriptor;
+}
+class Printer {
+    constructor() {
+        this._message = "This works!";
+    }
+    showMessage() {
+        console.log(this._message);
+    }
+}
+__decorate([
+    Autobind
+], Printer.prototype, "showMessage", null);
+const p = new Printer();
+p.showMessage();
+const button2 = document.querySelector("button");
+// karena addEventListener mengikat method showMessage sehingga this itu merefer ke addEventListerner
+button2 === null || button2 === void 0 ? void 0 : button2.addEventListener("click", p.showMessage);
