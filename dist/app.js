fbf8acd6f67c275810627d6822d450675eaa0eac
Muhammad Nizar Fazari <nexsel32@gmail.com>
2024-01-31 19:11:52 +0700

    Make a input value on inputElement

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,29 @@
 "use strict";
 class ProjectInput {
     constructor() {
+        // mengambil element html
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
+        // mennciptakan salinan node
         const importedNode = document.importNode(this.templateElement.content, true);
+        // mengambil element pertama saja
         this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputEl = this.element.querySelector('#title');
+        this.descriptionInputEl = this.element.querySelector('#description');
+        this.peopleInputEl = this.element.querySelector('#people');
+        this.configure();
         this.attach();
     }
+    submitHandler(e) {
+        e.preventDefault();
+        console.log(this.titleInputEl.value);
+    }
+    configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this));
+    }
     attach() {
+        // menyisipkan ke dalam hostElement yang idnya pada app
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 }
--- drag-n-drop/app.ts
+++ drag-n-drop/app.ts
@@ -2,16 +2,41 @@ class ProjectInput {
     templateElement: HTMLTemplateElement
     hostElement : HTMLDivElement
     element :HTMLFormElement
+
+    titleInputEl : HTMLInputElement
+    descriptionInputEl : HTMLInputElement
+    peopleInputEl : HTMLInputElement
     constructor() {
+        // mengambil element html
         this.templateElement = document.getElementById('project-input') as HTMLTemplateElement
         this.hostElement = document.getElementById('app') as HTMLDivElement
         
+        // mennciptakan salinan node
         const importedNode = document.importNode(this.templateElement.content, true)
+        // mengambil element pertama saja
         this.element = importedNode.firstElementChild as HTMLFormElement
+        this.element.id = 'user-input'
+
+        this.titleInputEl = this.element.querySelector('#title') as HTMLInputElement
+        this.descriptionInputEl = this.element.querySelector('#description') as HTMLInputElement
+        this.peopleInputEl = this.element.querySelector('#people') as HTMLInputElement
+
+        this.configure()
         this.attach()
     }
 
+    private submitHandler(e : Event){
+        e.preventDefault()
+        console.log(this.titleInputEl.value)
+
+    }
+
+    configure(){
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
+    }
+
     private attach(){
+        // menyisipkan ke dalam hostElement yang idnya pada app
         this.hostElement.insertAdjacentElement('afterbegin', this.element)
     }
 
